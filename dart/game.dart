11b67fec4539b4e8d275d7ceb82110df529ffe60
srdangelo <sarahdangelo92@gmail.com>
2014-03-09 17:07:05 -0500

    60 plays

--- dart/game.dart
+++ dart/game.dart
@@ -8,7 +8,7 @@ part of fwf;
 class Game {
   
   //turn off annoying transitions 0 = on; 1 = off
-  bool debugTransition = true;
+  bool debugTransition = false;
    
   // this is the HTML canvas element
   CanvasElement canvas;
